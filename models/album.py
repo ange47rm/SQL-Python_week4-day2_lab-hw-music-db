862636cdf21e8a2295fada6ec32501349657dc0e
ange47rm <ange47rm@hotmail.com>
2020-12-08 15:28:02 +0000

    Created file structure, artist and album classes

---
+++ models/album.py
@@ -0,0 +1,9 @@
+from artist import Artist
+
+class Album ():
+
+    def __init__(self, title, genre, artist, id = None):
+        self.title = title
+        self.genre = genre
+        self.artist = artist
+        self.id = id
